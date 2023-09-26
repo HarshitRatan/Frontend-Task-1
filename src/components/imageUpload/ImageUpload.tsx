6f097632ea5babb31c1cce7617e092f6dc2ed6b0
harshitratan <harshitratanshukla@gmail.com>
2023-09-26 11:00:17 +0530

    minor css fix

--- src/components/imageUpload/ImageUpload.tsx
+++ src/components/imageUpload/ImageUpload.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Space } from "antd";
+import { Button, Space } from "antd";
 import Title from "antd/es/typography/Title";
 import { UploadOutlined } from "@ant-design/icons";
 
@@ -7,65 +7,131 @@ const ImageUpload = () => {
   const [file, setFile] = React.useState("");
   const handleChange = (e: any) => {
     console.log(e.target.files);
-    setFile(URL.createObjectURL(e.target.files[0]));
+    try {
+      setFile(URL.createObjectURL(e.target.files[0]));
+    } catch (e) {
+      console.log("No File Selected");
+    }
+  };
+  const handleClick = () => {
+    document.getElementById("upload-tag")?.click();
   };
 
   return (
-    <Space
-      style={{
-        borderRadius: "5px",
-        border: "1px dashed #000",
-        boxShadow: "3px 3px 9px 0px rgba(190, 190, 190, 0.13)",
-        height: "15rem",
-        width: "30rem",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        marginTop: "5rem",
-      }}
-    >
+    <>
+      <input type="file" onChange={handleChange} id="upload-tag" hidden />
       {file.length === 0 && (
         <>
-          <UploadOutlined style={{ fontSize: "3rem" }} />
           <Title
             style={{
               color: "#000",
               fontFamily: "Poppins",
-              fontSize: "1rem",
+              fontSize: "1.5rem",
               fontStyle: "normal",
-              fontWeight: "600",
+              fontWeight: 600,
+              borderRadius: "1.2rem 1.2rem  0px 0px",
+              background: "#D0F7FA",
+              marginTop: 0,
+              padding: "1.2rem",
             }}
           >
             Upload cover image
           </Title>
-          <Title
+          <Space
             style={{
-              color: "#979797",
-              fontFamily: "Poppins",
-              fontSize: "0.9rem",
-              fontStyle: "normal",
-              fontWeight: "500",
+              backgroundColor: "white",
+              borderRadius: "1.2rem",
+              display: "flex",
+              alignItems: "stretch",
+              justifyContent: "center",
+              flexDirection: "column",
             }}
           >
-            16:9 ratio is recommended. Max image size 1mb
-          </Title>
-          <input type="file" onChange={handleChange} />
+            <Space
+              style={{
+                backgroundColor: "white",
+                borderRadius: "5px",
+                border: "1px dashed #000",
+                boxShadow: "3px 3px 9px 0px rgba(190, 190, 190, 0.13)",
+                height: "30%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                margin: "2.5rem",
+                padding: "10px",
+              }}
+              onClick={handleClick}
+            >
+              <UploadOutlined style={{ fontSize: "3rem" }} />
+              <Title
+                style={{
+                  color: "#000",
+                  fontFamily: "Poppins",
+                  fontSize: "1rem",
+                  fontStyle: "normal",
+                  fontWeight: "600",
+                }}
+              >
+                Upload Cover Image
+              </Title>
+              <Title
+                style={{
+                  color: "#979797",
+                  fontFamily: "Poppins",
+                  fontSize: "0.9rem",
+                  fontStyle: "normal",
+                  fontWeight: "500",
+                }}
+              >
+                16:9 ratio is recommended. Max image size 1mb
+              </Title>
+            </Space>
+          </Space>
         </>
       )}
       {file.length > 0 && (
-        <>
+        <Space
+          style={{
+            borderRadius: "5px",
+            boxShadow: "3px 3px 9px 0px rgba(190, 190, 190, 0.13)",
+            height: "15rem",
+            width: "30rem",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+          onClick={handleClick}
+        >
           <img
             src={file}
             alt="coding---123456"
             style={{
-              height: "15rem",
-              width: "30rem",
+              height: "100%",
+              width: "100%",
+              objectFit: "cover",
             }}
           />
-        </>
+          <Button
+            type="text"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: " #A80000",
+              fontFamily: "Poppins",
+              fontSize: "1rem",
+              fontStyle: "normal",
+              fontWeight: "600",
+              lineHeight: "1.5rem",
+            }}
+          >
+            ❌ Delete & re-upload
+          </Button>
+        </Space>
       )}
-    </Space>
+    </>
   );
 };
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import ImageUpload from "../../components/imageUpload/ImageUpload";
+import { Col, Row } from "antd";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <ImageUpload />
+      <Row style={{marginTop: "5rem" }}>
+        <Col
+          span={10}
+          style={{
+            backgroundColor: "white",
+            borderRadius: "1.2rem",
+            boxShadow: "3px 3px 14px 0px rgba(190, 190, 190, 0.30)",
+          }}
+        >
+          <ImageUpload />
+        </Col>
+      </Row>
     </div>
   );
 };
